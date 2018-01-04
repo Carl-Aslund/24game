c7488e65b249deaf6e6378295eeb345853e2d78d
Carl Aslund <caslund@g.hmc.edu>
2018-01-04 18:01:57 -0500

    Add prototype code for the game solver (WIP)

---
+++ 24solver.py
@@ -0,0 +1,55 @@
+from copy import deepcopy
+
+def add(x, y):
+    """Basic addition operator."""
+    return x+y
+
+def sub(x, y):
+    """Basic subtraction operator."""
+    return x-y
+
+def mult(x, y):
+    """Basic addition operator."""
+    return x*y
+
+def div(x, y):
+    """Divide-by-zero safe division operator."""
+    if y==0: # Defaults to addition/subtraction, since division is illegal
+        return x # Might need to edit of an actual solution is desired
+    return x/y
+
+def isSolvable(nums):
+    """Determines if an input list of numbers can be used to make 24."""
+    length = len(nums)
+    if length < 1:
+        return False # This should not 
+    if length == 1:
+        return nums[0] == 24 # Base case
+    # Will only continue if there are 2 or more numbers
+    funcs = [add, sub, mult, div]
+    for i in range(length):
+        for j in range(1,length):
+            # Create a deepcopy of the available numbers with two missing
+            firstNum = nums[i]
+            secondNum = nums[j]
+            numsCopy = deepcopy(nums)
+            numsCopy.remove(firstNum)
+            numsCopy.remove(secondNum)
+            # Permute the result
+            for f in funcs:
+                optionA = f(firstNum, secondNum)
+                optionB = f(secondNum, firstNum)
+                resultA = isSolvable(numsCopy + [optionA])
+                resultB = isSolvable(numsCopy + [optionB])
+                if resultA or resultB:
+                    return True
+    # Will only continue if no solution was found
+    return False
+                
+
+
+
+
+
+
+                
